d3988234b37ecf4ebe87825d6185242ae81958d4
arsy <arsykkirgj@gmail.com>
2021-03-27 21:15:39 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello', function () {
+    return 'selamat pagi dunia, namaku arsy';
+});
+Route::get('/hallo',function () {
+    echo '<h1> selamat sejahtera dunia </h1>';
+    echo '<p> nama saya mohammad arsy jahfal, mahasiswa di Poilteknik negeri Jember.';
+});
